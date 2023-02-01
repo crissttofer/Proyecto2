0286e4b4988d79b8619bc078926b97537f61f780
crissttofer <97007526+crissttofer@users.noreply.github.com>
2023-02-01 18:35:26 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace Proyecto2{
+  class Program{
+    public static void Main(string[] args){
+    }
+  }
+}
